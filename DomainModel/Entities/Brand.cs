39697ef2d70f330944a17e0a22e35467a67f053e
Яна Литвинчик <yana.litvinchik@gmail.com>
2020-01-28 01:39:55 +0200

    Add project files.

---
+++ DomainModel/Entities/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Good> Good { get; set; } = new List<Good>();
+    }
+}
---
+++ DomainModel/Entities/Good.cs
@@ -0,0 +1,32 @@
+﻿namespace DomainModel.Entities
+{
+    public class Good
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+        public Sex Sex { get; set; }
+        public string Type { get; set; }
+
+        public int Count { get; set; }
+
+        public double Rating { get; set; }
+
+        public int? ManufacturerId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int? BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+
+    public enum Sex
+    {
+        Girl = 1,
+        Boy = 2
+    }
+}
